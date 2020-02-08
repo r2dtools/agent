9bb1acf19ff4244f8bf9ac9f33f550fd3330d769
Alexandr Tikhonov <atikhonov@plesk.com>
2020-02-08 21:11:25 +0700

    Added simple cli skeleton

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/alexsibtihon/r2st-agent
+
+go 1.13
+
+require github.com/spf13/cobra v0.0.5
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/alexsibtihon/r2st-agent/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}

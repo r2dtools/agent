00834ba041bdb66a986cb192452637d7fa49b29c
Alexander Tikhonov <alex.vl.tihon@gmail.com>
2020-10-16 00:24:38 +0700

    Fixed path to .version file

--- utils/version.go
+++ utils/version.go
@@ -3,12 +3,16 @@ package utils
 import (
 	"fmt"
 	"io/ioutil"
+	"path/filepath"
 	"strings"
+
+	"github.com/r2dtools/agent/config"
 )
 
 // GetAgentVersion returns agent version
 func GetAgentVersion() (string, error) {
-	output, err := ioutil.ReadFile(".version")
+	config := config.GetConfig()
+	output, err := ioutil.ReadFile(filepath.Join(config.ExecutablePath, ".version"))
 
 	if err != nil {
 		return "", fmt.Errorf("could not detect agent version: %v", err)
